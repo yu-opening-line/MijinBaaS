d29d2de1f195054ab7e4591f308c1ca5e70dc1e4
yu000 <kane@opening-line.co.jp>
2018-01-31 19:45:35 +0900

    mod

--- scripts/install_mijin.sh
+++ scripts/install_mijin.sh
@@ -27,38 +27,3 @@ sudo firewall-cmd --reload
 sudo systemctl enable firewalld.service
 
 cd $INSTALL_HOME
-
-INSTALLER=`echo  $MIJIN_INSTALLER_URL | sed -r 's/^.*(mijin\..*\.tar\.gz)\?.*$/\1/'`
-
-wget $MIJIN_INSTALLER_URL -O $INSTALLER
-
-tar zxvf $INSTALLER
-
-rm -f $INSTALLER
-
-cd mijin
-tar zxvf serverjars.tgz
-
-IP=`curl ifconfig.co`
-sed -e "s/%IP%/$MIJIN_HOST_1/" config-user.properties.org >config-user.properties.ip
-sed -e "s/%AddItionalLocalIps%/$ADDITIONAL_LOCAL_IPS/" config-user.properties.ip >config-user.properties
-
-sed -e "s/%IP1%/$MIJIN_HOST_1/" peers-config_mijinnet.json.org >peers-config_mijinnet.json.ip1
-sed -e "s/%IP2%/$MIJIN_HOST_2/" peers-config_mijinnet.json.ip1 >peers-config_mijinnet.json.ip2
-sed -e "s/%IP3%/$MIJIN_HOST_3/" peers-config_mijinnet.json.ip2 >peers-config_mijinnet.json.ip3
-sed -e "s/%IP4%/$MIJIN_HOST_4/" peers-config_mijinnet.json.ip3 >peers-config_mijinnet.json.ip4
-sed -e "s/%IP5%/$MIJIN_HOST_5/" peers-config_mijinnet.json.ip4 >peers-config_mijinnet.json
-
-rm -f config-user.properties.org
-rm -f config-user.properties.ip
-
-rm -f peers-config_mijinnet.json.org
-rm -f peers-config_mijinnet.json.ip1
-rm -f peers-config_mijinnet.json.ip2
-rm -f peers-config_mijinnet.json.ip3
-rm -f peers-config_mijinnet.json.ip4
-
-DIR=`pwd`
-echo "@reboot screen -S mijin -d -m $DIR/startnem.sh" | sort - | uniq - | crontab -
-
-sudo screen -S mijin  -d -m $DIR/startnem.sh
